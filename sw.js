cc554f46fca06d2630c2a8d411d9f8028ebffa8d
Vivek ghinaiya <56452603+vivekghinaiya@users.noreply.github.com>
2023-11-11 18:05:04 +0530

    update 1.2

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1699705012';
+const cacheName = 'chirpy-1699706035';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
