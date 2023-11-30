ae8bca306100111025ed22f94dbb2770452733d9
Vivek ghinaiya <56452603+vivekghinaiya@users.noreply.github.com>
2023-11-30 20:22:50 +0530

    dsfds
    fsdf

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1701355202';
+const cacheName = 'chirpy-1701355872';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
