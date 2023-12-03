06c13afda0a98be8b83d9181dd7bedc5976b65b3
Vivek ghinaiya <56452603+vivekghinaiya@users.noreply.github.com>
2023-12-03 16:50:33 +0530

    s
    s

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1701506953';
+const cacheName = 'chirpy-1701602361';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
